980d0c82a4b50d726b35482268d97fd43ef83979
self-mismatch <44555577+self-mismatch@users.noreply.github.com>
2021-02-17 22:50:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,35 +2,35 @@ const openBrackets = ['(', '{', '['];
 const closeBrackets = [')', '}', ']'];
 
 const Bracket = {
-	'(': ')',
-  '{': '}',
-  '[': ']',
+    '(': ')',
+    '{': '}',
+    '[': ']',
 };
 
 const isOpenBracket = (bracket) => {
-	return openBrackets.find((openBracket) => openBracket === bracket) ? true : false;
+    return openBrackets.find((openBracket) => openBracket === bracket) ? true : false;
 };
 
 const isCloseBracket = (bracket) => {
-	return closeBrackets.find((closeBracket) => closeBracket === bracket) ? true : false;
+    return closeBrackets.find((closeBracket) => closeBracket === bracket) ? true : false;
 };
 
 const checkCorrectBrackets = (string) => {
-	let brackets = [];
-	let isCorrect = true;
-	
-  for (let i = 0; i < string.length; i++) {
-  	if (isOpenBracket(string[i])) {
-    	brackets.push(string[i]);
-    } else if (isCloseBracket(string[i])) {
-    	const lastOpenBracket = brackets.pop();
-      
-      if (Bracket[lastOpenBracket] !== string[i]) {
-      	isCorrect = false;
-        break;
-      }
+    let brackets = [];
+    let isCorrect = true;
+
+    for (let i = 0; i < string.length; i++) {
+        if (isOpenBracket(string[i])) {
+            brackets.push(string[i]);
+        } else if (isCloseBracket(string[i])) {
+            const lastOpenBracket = brackets.pop();
+
+            if (Bracket[lastOpenBracket] !== string[i]) {
+                isCorrect = false;
+                break;
+            }
+        }
     }
-  }
-  
-  return isCorrect;
+
+    return isCorrect;
 };
